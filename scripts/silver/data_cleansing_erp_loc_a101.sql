83999fd5080476337252f36cabf0907e93117ba7
magaly-boop <magalypratt@gmail.com>
2026-07-22 14:42:35 -0500

    Create data_cleansing_erp_loc_a101.sql

---
+++ scripts/silver/data_cleansing_erp_loc_a101.sql
@@ -0,0 +1,9 @@
+INSERT INTO silver.erp_loc_a101 (cid, cntry)
+SELECT 
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+	ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101
